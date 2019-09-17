2576fc7f7621c64cd1272fa0d21ff5b7004680a6
Maria Castro <mariacantocastro@gmail.com>
2019-09-18 00:40:45 +0200

    ...

--- api/locations.go
+++ api/locations.go
@@ -1,1 +1,61 @@
 package api
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+	"github.com/mariacastro96/go_quiz/locations"
+)
+
+// AddLocationHandler adds location
+// type mc struct {
+// 	name string
+// 	age  int
+// }
+
+func AddLocationHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+
+		var data locations.Location
+
+		err := decoder.Decode(&data)
+		if err != nil {
+			panic(err)
+		}
+
+		locations := make(chan locations.Location, 1000)
+
+		log.Printf("locations %T", locations)
+		// go log.Println(maria)
+
+		err = db.Ping()
+		if err != nil {
+			fmt.Fprintf(w, "Having DB problems, will save your data as soon as possible")
+			select {
+			case locations <- data:
+				log.Println("received location", data)
+				log.Println("LOCATIONS", len(locations))
+			default:
+				log.Println("no location received")
+			}
+
+		} else {
+			log.Println("LOCATIONS", len(locations))
+			lastInsertId := 0
+			err = db.QueryRow("INSERT INTO locations (lat, lon, driver_id) VALUES ($1, $2, $3) RETURNING id", data.Lat, data.Lon, data.DriverID).Scan(&lastInsertId)
+			if err != nil {
+				log.Fatal("QUERY ERROR", err)
+
+			} else {
+				log.Printf("lat: %v, lon: %v, driver id: %v, id: %v", data.Lat, data.Lon, data.DriverID, lastInsertId)
+
+				fmt.Fprintf(w, "OK! location id: %v", lastInsertId)
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -2,45 +2,32 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
-	"github.com/mariacastro96/go_quiz/locations"
+	"github.com/mariacastro96/go_quiz/api"
 )
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-
-	var data locations.Location
-	err := decoder.Decode(&data)
-	if err != nil {
-		panic(err)
-	}
-
-	log.Printf("lat: %v, lon: %v, driver id: %v", data.Lat, data.Lon, data.DriverID)
-	fmt.Fprintf(w, "lat: %v, lon: %v, driver id: %v", data.Lat, data.Lon, data.DriverID)
-}
-
 func main() {
-	db, err := sql.Open("postgres", "postgres://postgres:password@localhost/quizlocations?sslmode=disable")
+	db, err := sql.Open("postgres", "postgres://postgres:password@localhost/quiz_locations?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Error: Could not establish a connection with the database")
-	}
+	// err = db.Ping()
+	// if err != nil {
+	// 	log.Println("this is not ok with the DB")
+	// 	// log.Fatal("Error: Could not establish a connection with the database. err: %d", err.Error)
+	// }
 	defer db.Close()
 
 	log.Println("go")
 	fmt.Println("db ok")
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/locations", homeLink).Methods("POST")
+	router.HandleFunc("/locations", api.AddLocationHandler(db)).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }

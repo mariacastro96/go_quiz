4218f2f1875d66f7b6588fb52b1ca75aafb5fb04
mariacastro96 <mariacantocastro@hmail.com>
2019-09-26 11:27:31 +0200

    happy path

--- api/locations.go
+++ api/locations.go
@@ -12,7 +12,7 @@ import (
 )
 
 // AddLocationHandler decodes the json sent by client and answers to the client
-func AddLocationHandler(locs storage.Postgres, storedLocs []locations.Location) func(http.ResponseWriter, *http.Request) {
+func AddLocationHandler(store storage.Postgres) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		decoder := json.NewDecoder(r.Body)
 
@@ -20,25 +20,20 @@ func AddLocationHandler(locs storage.Postgres, storedLocs []locations.Location) 
 
 		err := decoder.Decode(&data)
 		if err != nil {
-			panic(err)
+			log.Println("DECODE ERROR", err)
 		}
 
 		data.ID = uuid.New()
 
-		storedLocs = append(storedLocs, data)
-		log.Println("Locs before", storedLocs)
+		data, err = store.Insert(data)
+		if err != nil {
+			log.Println("QUERY ERROR", err)
 
-		for _, v := range storedLocs {
-			data, err = locs.Insert(v)
-			if err != nil {
-				log.Println("QUERY ERROR", err)
+			fmt.Fprintf(w, "location id: %v, \nlatitude: %v, \nlongitude: %v, \ndriver id: %v", data.ID, data.Lat, data.Lon, data.DriverID)
+		} else {
 
-				fmt.Fprintf(w, "location id: %v, \nlatitude: %v, \nlongitude: %v, \ndriver id: %v", data.ID, data.Lat, data.Lon, data.DriverID)
-			} else {
-				storedLocs = storedLocs[1:]
-				log.Println("Locs after", storedLocs)
-				fmt.Fprintf(w, "location id: %v, \nlatitude: %v, \nlongitude: %v, \ndriver id: %v", data.ID, data.Lat, data.Lon, data.DriverID)
-			}
+			fmt.Fprintf(w, "location id: %v, \nlatitude: %v, \nlongitude: %v, \ndriver id: %v", data.ID, data.Lat, data.Lon, data.DriverID)
 		}
+
 	}
 }
--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"github.com/mariacastro96/go_quiz/api"
-	"github.com/mariacastro96/go_quiz/locations"
 	"github.com/mariacastro96/go_quiz/storage"
 )
 
@@ -19,18 +17,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	locs := storage.Postgres{
+	defer db.Close()
+
+	locationsStore := storage.Postgres{
 		DB: db,
 	}
 
-	var storedLocations []locations.Location
-
-	defer db.Close()
-
-	log.Println("go")
-	fmt.Println("db ok")
-
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/locations", api.AddLocationHandler(locs, storedLocations)).Methods("POST")
+	router.HandleFunc("/locations", api.AddLocationHandler(locationsStore)).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
